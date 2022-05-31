dcfb03c2ce8fb0b0f0cd0d7fd980b84d8a26a6b1
LeoBang <bluke8489@gmail.com>
2022-05-31 12:10:54 +0900

    initial setting Java Proj

---
+++ src/main/java/com/appledeveloperacademy/MC2Server/Mc2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.appledeveloperacademy.MC2Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mc2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mc2ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appledeveloperacademy/MC2Server/Mc2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appledeveloperacademy.MC2Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mc2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
