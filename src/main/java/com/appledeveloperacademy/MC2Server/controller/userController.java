341d4772b61041a5136e2383901682aa6c85847e
LeoBang <bluke8489@gmail.com>
2022-06-08 11:05:48 +0900

    feat: log/room/user controller endpoint

--- src/main/java/com/appledeveloperacademy/MC2Server/controller/userController.java
+++ src/main/java/com/appledeveloperacademy/MC2Server/controller/userController.java
@@ -1,2 +1,22 @@
-package com.appledeveloperacademy.MC2Server.controller;public class userController {
+package com.appledeveloperacademy.MC2Server.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/users")
+@RestController
+public class userController {
+
+    @PostMapping
+    public String createUser(@RequestBody final CreateUserReq createUserReq) {
+        return "createUser";
+    }
+
+    @GetMapping("/{usercode}")
+    public String getUser(@PathVariable final String usercode) {
+        return "getUser";
+    }
+
+    static class CreateUserReq {
+        private String username;
+    }
 }
