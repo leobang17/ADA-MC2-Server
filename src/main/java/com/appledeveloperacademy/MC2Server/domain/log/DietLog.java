1a6375ae13fb95fd6960e5c09f1540b9106d5d95
LeoBang <bluke8489@gmail.com>
2022-06-10 17:01:16 +0900

    feat: override createdAt column

--- src/main/java/com/appledeveloperacademy/MC2Server/domain/log/DietLog.java
+++ src/main/java/com/appledeveloperacademy/MC2Server/domain/log/DietLog.java
@@ -3,8 +3,10 @@ package com.appledeveloperacademy.MC2Server.domain.log;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.time.LocalDateTime;
 
 @Entity
 @DiscriminatorValue(value = "D")
@@ -12,4 +14,7 @@ import javax.persistence.Entity;
 public class DietLog extends Log {
     private String name;
     private int amount;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/appledeveloperacademy/MC2Server/domain/log/WaterLog.java
+++ src/main/java/com/appledeveloperacademy/MC2Server/domain/log/WaterLog.java
@@ -3,12 +3,17 @@ package com.appledeveloperacademy.MC2Server.domain.log;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.time.LocalDateTime;
 
 @Entity
 @DiscriminatorValue(value = "W")
 @Getter @Setter
 public class WaterLog extends Log{
     private int amount;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 }
