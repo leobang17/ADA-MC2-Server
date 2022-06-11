72210bd14324131852464e9258ccf70b498fcee2
LeoBang <bluke8489@gmail.com>
2022-06-11 17:23:43 +0900

    test: Respository listAllHealthTags

--- src/test/java/com/appledeveloperacademy/MC2Server/repository/UserRepositoryTest.java
+++ src/test/java/com/appledeveloperacademy/MC2Server/repository/UserRepositoryTest.java
@@ -1,17 +1,18 @@
 package com.appledeveloperacademy.MC2Server.repository;
 
+import com.appledeveloperacademy.MC2Server.domain.HealthTag;
 import com.appledeveloperacademy.MC2Server.domain.Member;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,15 +28,12 @@ class UserRepositoryTest {
     @Test
     void save() {
         // given
-        Member member = new Member();
-        member.setUsername("leo");
-        String s = UUID.randomUUID().toString();
-        member.setUsercode(s);
+        Member member = createMember();
         userRepository.save(member);
         Long id = member.getId();
 
         // when
-//        em.flush();
+        em.flush();
         Member byId = userRepository.findById(id);
 
         // then
@@ -45,16 +43,43 @@ class UserRepositoryTest {
     @Test
     void findByUsercode() {
         // given
-        Member member = new Member();
-        member.setUsername("leo");
-        String s = UUID.randomUUID().toString();
-        member.setUsercode(s);
+        Member member = createMember();
         userRepository.save(member);
 
         // when
-        Member byUsercode = userRepository.findByUsercode(s);
+        Member byUsercode = userRepository.findByUsercode(member.getUsercode());
 
         // then
         assertEquals(member.getUsercode(), byUsercode.getUsercode());
     }
+
+    @Test
+    void listTags() {
+        // given 
+        Member member = createMember();
+        HealthTag tag1 = new HealthTag();
+        tag1.setContent("뿡");
+        HealthTag tag2 = new HealthTag();
+        tag2.setContent("뽕");
+        tag1.addHealthTag(member);
+        tag2.addHealthTag(member);
+        em.persist(member);
+        
+        // when
+        Long id = member.getId();
+        List<HealthTag> tags = userRepository.listHealthTagsById(id);
+
+        // then
+//        assertEquals(member.getHealthTags(), tags);
+
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setUsername("leo");
+        String s = UUID.randomUUID().toString();
+        member.setUsercode(s);
+        return member;
+    }
+
 }
