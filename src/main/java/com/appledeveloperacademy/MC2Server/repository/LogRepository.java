acdfaa356d8ad0f459b5e31ed96253b20a61b615
LeoBang <bluke8489@gmail.com>
2022-06-12 16:56:04 +0900

    feat: add LogRepository findLogics

--- src/main/java/com/appledeveloperacademy/MC2Server/repository/LogRepository.java
+++ src/main/java/com/appledeveloperacademy/MC2Server/repository/LogRepository.java
@@ -2,10 +2,13 @@ package com.appledeveloperacademy.MC2Server.repository;
 
 import com.appledeveloperacademy.MC2Server.domain.log.DietLog;
 import com.appledeveloperacademy.MC2Server.domain.log.Log;
+import com.appledeveloperacademy.MC2Server.domain.log.SnackLog;
+import com.appledeveloperacademy.MC2Server.domain.log.WaterLog;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.swing.*;
 import java.util.List;
 
 @Repository
@@ -20,6 +23,45 @@ public class LogRepository {
                 .setFirstResult(offset)
                 .setMaxResults(limit)
                 .getResultList();
+    }
+
+    public DietLog findDietLogByRoomId(Long roomId, boolean isPrivate) {
+        return em.createQuery(
+                        "SELECT l" +
+                                " FROM DietLog l" +
+                                " JOIN l.room r" +
+                                " JOIN FETCH l.member m" +
+                                " WHERE r.id = :id" +
+                                " AND l.isPublic = :public", DietLog.class)
+                .setParameter("id", roomId)
+                .setParameter("public", !isPrivate)
+                .getSingleResult();
+    }
+
+    public WaterLog findWaterLogByRoomId(Long roomId, boolean isPrivate) {
+        return em.createQuery(
+                        "SELECT l" +
+                                " FROM WaterLog l" +
+                                " JOIN l.room r" +
+                                " JOIN FETCH l.member m" +
+                                " WHERE r.id = :id" +
+                                " AND l.isPublic = :public", WaterLog.class)
+                .setParameter("id", roomId)
+                .setParameter("public", !isPrivate)
+                .getSingleResult();
+    }
+
+    public SnackLog findSnackLogByRoomId(Long roomId, boolean isPrivate) {
+        return em.createQuery(
+                        "SELECT l" +
+                                " FROM SnackLog l" +
+                                " JOIN l.room r" +
+                                " JOIN FETCH l.member m" +
+                                " WHERE r.id = :id" +
+                                " AND l.isPublic = :public", SnackLog.class)
+                .setParameter("id", roomId)
+                .setParameter("public", !isPrivate)
+                .getSingleResult();
     }
 
     private String logQueryGenerator(Long roomId, LogType type) {
